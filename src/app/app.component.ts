e9cb7f2fbc8679eaf029234782c14f5ecb317196
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-08-28 15:58:23 +0200

    make the first request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { Injectable } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+
+  styleUrls: ['./app.component.css'],
+
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+  title = 'Star Wars';
+  results: any;
+  resultatEnvoi : any;
+
+
+// Inject HttpClient into your component or service.
+constructor(private http: HttpClient) {}
+
+ngOnInit(): void {
+  // Make the HTTP request:
+  this.http.get('https://swapi.co/api/').subscribe(data => {
+    // Read the result field from the JSON response.
+    this.results = Object.keys(data);
+    console.table(this.results);
+  });
+}
+envoi(test) {
+  this.http.get('https://swapi.co/api/' + test).subscribe(data => {
+    // Read the result field from the JSON response.
+    this.resultatEnvoi = data['results'];
+
+
+  });
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+
+import {HttpClientModule} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
